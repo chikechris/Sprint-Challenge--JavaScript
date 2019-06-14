b3125c7d64198a1bfb790bf6534d4cd82db49f19
Chike Chris <cchike2017@gmail.com>
2019-06-14 13:30:06 -0400

    testing objects-arrays.js file

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -254,24 +254,34 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
-console.log(lowerCase);
+//const lowerCase = [];
+const lowerCase = zooAnimals.map(item => {
+  return item.animal_name.toLowerCase();
+});
+
+console.log("Animal Names:", lowerCase);
 
 /* Request 3: .filter() 
 
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const lowerPopulation = [];
-console.log(lowerPopulation);
+//const lowerPopulation = [];
+const lowerPopulation = zooAnimals.filter(item => {
+  return item.population < 5;
+});
+console.log("Low Population:", lowerPopulation);
 
 /* Request 4: .reduce() 
 
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
-console.log(populationTotal);
+const populationTotal = zooAnimals.reduce((acc, item) => {
+  return acc + item.population;
+}, 0);
+
+console.log("Population Total:", populationTotal);
 
 /* 
 
